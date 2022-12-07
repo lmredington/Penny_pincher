beba3f5785d3b42d9f036a5e9bf9bcac97ebc3a8
lmredington <119895386+lmredington@users.noreply.github.com>
2022-12-07 17:52:28 -0600

    Create dashboard.php

---
+++ penny_pincher/dashboard.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+ <meta charset="utf-8"> 
+ <title>Budget App</title>
+ <link rel="stylesheet" href="style.css"> 
+</head> 
+<body> 
+  
+    <h1>Dashboard</h1>
+    <br> </br>
+
+ <br> </br>
+
+ <form method = "POST" action ="purchaseReport.php"> 
+  <input type = "submit" value="View Purchases"/> 
+ </form> 
+
+ <br></br>
+
+  <form method = "POST" action ="addPurchase.php"> 
+  <input type = "submit" value="Add Purchase"/> 
+ </form> 
+
+<br></br>
+ 
+  <form method = "POST" action ="addIncome.php"> 
+  <input type = "submit" value="Add Income"/> 
+ </form> 
+
+<br></br>
+ 
+  <form method = "POST" action ="incomehistory.php"> 
+  <input type = "submit" value="View Income History"/> 
+ </form> 
+ 
+ <br></br>
+ <form method = "POST" action ="editProfile.php"> 
+  <input type = "submit" value="Edit Profile"/> 
+  </form>
+ 
+ <br></br>
+ <form method = "POST" action ="logout.php"> 
+  <input type = "submit" value="Logout"/> 
+  </form>
+
+</body> 
+</html>
