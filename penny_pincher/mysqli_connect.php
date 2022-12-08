2db3762d902fdba257c53a8cd4d6b110ed5e08d4
lmredington <119895386+lmredington@users.noreply.github.com>
2022-12-07 18:45:51 -0600

    Update mysqli_connect.php

--- penny_pincher/mysqli_connect.php
+++ penny_pincher/mysqli_connect.php
@@ -7,7 +7,7 @@
  DEFINE ('DB_HOST', 'localhost'); 
  DEFINE ('DB_USER', 'root'); 
  DEFINE ('DB_PASSWORD', ''); 
- DEFINE ('DB_NAME', 'budget'); 
+ DEFINE ('DB_NAME', 'penny_pincher'); 
  // $dbc will contain a resource link to the database 
  // @ keeps the error from showing in the browser 
  $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) 
